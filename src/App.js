d73f12cb9494c8745aa5a388af7b1913a3524ea5
Jacqueline Silva <jacque.sx@hotmail.com>
2022-03-08 15:29:04 -0300

    Requisito 05 - Crie o formulário para pesquisar artistas

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
 
     this.state = {
       nameInput: '',
+      nameArtist: '',
       loading: false,
       redirect: false,
     };
@@ -41,12 +42,13 @@ class App extends React.Component {
 
   inputChange({ target }) {
     this.setState({
-      nameInput: target.value,
+      [target.name]: target.value,
     });
+    console.log(target.name, target.value);
   }
 
   render() {
-    const { nameInput, loading, redirect } = this.state;
+    const { nameInput, loading, redirect, nameArtist } = this.state;
 
     return (
       <>
@@ -64,7 +66,12 @@ class App extends React.Component {
             />
           </Route>
 
-          <Route path="/search" component={ Search } />
+          <Route path="/search">
+            <Search
+              inputChange={ this.inputChange }
+              nameArtist={ nameArtist }
+            />
+          </Route>
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile" component={ Profile } exact />
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
   render() {
+    const { inputChange, nameArtist } = this.props;
+    const min = 2;
+
     return (
       <div data-testid="page-search">
-        Search
+        <h3>Search</h3>
+        <label htmlFor="searchArtist">
+          <input
+            name="nameArtist"
+            data-testid="search-artist-input"
+            onChange={ inputChange }
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ nameArtist.length < min }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
 }
+Search.propTypes = {
+  nameArtist: PropTypes.string.isRequired,
+  inputChange: PropTypes.func.isRequired,
+  // handleClick: PropTypes.func.isRequired,
+};
 
 export default Search;
